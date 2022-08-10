6c21eae6099a6104390390f0391c63e9d4cb15f0
jlquiroscass <jl.quiros.cassillas@gmail.com>
2022-08-10 17:16:49 +0200

    2022

--- src/main/java/net/celicidad/cangas/model/Tipo.java
+++ src/main/java/net/celicidad/cangas/model/Tipo.java
@@ -7,12 +7,14 @@ public class Tipo {
     public static String RESTAURANTE = "Restaurante";
     public static String RESTAURANTE_ALOJAMIENTO = "Alojamiento/Restaurante";
     public static String TIENDA = "Tienda";
+    public static String ACTIVIDAD = "Actividad";
 
     private static HashMap<Long, String> tipos = new HashMap<Long, String>(){{
         put(0L, ALOJAMIENTO);
         put(1L, RESTAURANTE);
         put(2L, TIENDA);
         put(3L, RESTAURANTE);
+        put(4L, ACTIVIDAD);
     }} ;
 
     public static HashMap<Long, String> getTipos() {
